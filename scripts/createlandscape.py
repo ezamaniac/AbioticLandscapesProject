ba010a505920355cefc2862fdf7b7234d9787bdb
EzaiyahJolly <ezlego2013@gmail.com>
2026-09-04 10:42:09 -0400

    restructured the repo so I can better include the organization for my scripts and other parts of the project

---
+++ scripts/createlandscape.py
@@ -0,0 +1,10 @@
+"""
+Code to create a landscape model using the midpoint displacement method as described by 
+Saupe (1988). This generated a landscape with a desired fractal dimension and roughness.
+@Params:
+    Dimension: size of the landscape (2D array)
+    Fractal dimension: desired fractal dimension of the landscape (between 2 and 3)
+    Hurst exponent: desired roughness of the landscape (between 0 and 1)
+    Final Standard Deviation: desired final standard deviation of the landscape
+    Additive succession: 
+"""
